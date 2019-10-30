4a41e0700f9079c38d354eddedb2b354f5cabbbe
spirrello <spirrello@opentext.com>
2019-10-30 12:50:24 -0400

    runCommand function

--- main.go
+++ main.go
@@ -3,15 +3,14 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"kcapture/models"
 	"log"
 	"net/smtp"
 	"os"
 	"os/exec"
+	"runtime"
 	"strconv"
-	"strings"
 	"time"
 )
 
@@ -44,34 +43,66 @@ func LogMessage(logLevel, message string) {
 
 }
 
-//pingTest will run test pings to a destination and report on packet loss
-func pingTest(destination string, count string) error {
-	log.Println("starting ping...")
-	cmd := exec.Command("ping", destination, "-c "+count)
+//runCommand runs the ping test and greps appropriately for packet loss
+func runCommand(commandApp string, args []string) (string, string, error) {
+	log.Println(args)
+	cmd := exec.Command(commandApp, args...)
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 	err := cmd.Run()
-	output, errStr := string(stdout.Bytes()), string(stderr.Bytes())
+
+	if err != nil {
+		return "", "", err
+	}
+
+	return string(stdout.Bytes()), string(stderr.Bytes()), nil
+
+}
+
+//pingTest will run test pings to a destination and report on packet loss
+func pingTest(destination string, count string) (string, error) {
+	log.Println("starting ping...")
+
+	var output, errStr, grepStr string
+	var err error
+	if runtime.GOOS == "OS darwin" {
+		grepStr = " | grep loss | awk '{print $7}'"
+
+	} else {
+		grepStr = " | grep loss | awk '{print $6}'"
+	}
+
+	args := []string{destination, "-c " + count + grepStr}
+	output, errStr, err = runCommand("ping", args)
+
+	//runCommand(destination, count, grepStr)
+	// cmd := exec.Command("ping", destination, "-c "+count+"| grep loss | awk '{print $7}'")
+	// cmd := exec.Command("ping", destination, "-c "+count)
+	// var stdout, stderr bytes.Buffer
+	// cmd.Stdout = &stdout
+	// cmd.Stderr = &stderr
+	// err := cmd.Run()
+	// output, errStr := string(stdout.Bytes()), string(stderr.Bytes())
 
 	//log errors and return
 	if err != nil {
-		LogMessage("ERROR", output+"\n"+errStr)
-		return err
-	} else if strings.Contains(string(output), "timeout") {
-		LogMessage("ERROR", output+"\n"+errStr)
-		err := errors.New(output + "\n" + errStr)
-		return err
-	} else if strings.Contains(string(output), "Destination Host Unreachable") {
-		LogMessage("ERROR", output+"\n"+errStr)
-		err := errors.New(output + "\n" + errStr)
-		return err
+		LogMessage("ERROR", errStr)
+		return "", err
 	}
+	// } else if strings.Contains(string(output), "timeout") {
+	// 	LogMessage("ERROR", output+"\n"+errStr)
+	// 	err := errors.New(output + "\n" + errStr)
+	// 	return err
+	// } else if strings.Contains(string(output), "Destination Host Unreachable") {
+	// 	LogMessage("ERROR", output+"\n"+errStr)
+	// 	err := errors.New(output + "\n" + errStr)
+	// 	return err
+	// }
 
-	LogMessage("INFO", "no packet loss")
-	LogMessage("INFO", string(output))
+	LogMessage("INFO", "packet loss:"+string(output))
 
-	return nil
+	return output, nil
 }
 
 //sendeEmail will send email alerts
@@ -118,18 +149,24 @@ func main() {
 	if loop != 0 {
 		for i := 0; i < loop; i++ {
 
-			err := pingTest(destination, count)
+			pingResult, err := pingTest(destination, count)
 			if err != nil {
 				LogMessage("ERROR", err.Error())
 				sendEmail("ERROR sending pings")
 			}
+			if pingResult != "0%" {
+				sendEmail("packet list: " + pingResult)
+			}
 		}
 	} else {
 		for {
-			err := pingTest(destination, count)
+			pingResult, err := pingTest(destination, count)
 			if err != nil {
 				LogMessage("ERROR", err.Error())
 				sendEmail("ERROR sending pings")
+			} else if pingResult != "0%" {
+				LogMessage("ERROR", err.Error())
+				sendEmail("packet loss: " + pingResult)
 			}
 			time.Sleep(5 * time.Second)
 		}
